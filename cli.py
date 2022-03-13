910f4257e31161b6373c392973ccb5fa7d63f010
Nick <n.drobyshevski@pm.me>
2022-03-13 14:28:10 +0100

    Added: Start prompt

---
+++ cli.py
@@ -0,0 +1,168 @@
+from rich.console import Console
+from rich.prompt import Prompt, Confirm
+from rich.panel import Panel
+from rich.padding import Padding
+
+from rich.layout import Layout
+from rich import print
+from random import *
+import os 
+import sys
+import time
+from exc import CustomException
+
+
+console = Console(highlight=False)
+
+
+def get_console_size():
+    '''returns current console size'''
+    size =  os.get_terminal_size()
+    cols = size.columns
+    lines = size.lines
+    return cols, lines
+
+cols, lines = get_console_size()
+width = cols // 8 * 4
+height = lines // 8  * 6
+
+
+def compose_string(s, *args):
+    '''returns string s with %var% replaced by arguments from *args.'''
+    
+    # console.log("compose string"," //", s, "// ",args[0]," //")
+    if args:
+        for a in args:
+            s = s.replace("%var%", a, 1)
+            # console.log('replaced //', s, '//')
+    else:
+        raise CustomException("args are required")
+    # console.log("compose string", "//",s,"//", ' -- return')
+    return s
+
+
+# def glitcher(msg, chars):
+#     '''prints msg with glitch effect'''
+#     for let in msg:
+
+
+
+def get_padding(msg, allign='t', justify='r'):
+    '''
+        returns padding size
+            msg -message needs for calculate message length
+            allign - vertical padding [t:top, c:center, b:bottom]
+            justify - vertical padding [l:left, c:center, r:right].
+       '''
+    cols, lines = get_console_size()
+    if allign == 'c':
+        line_coeff = (lines - 3)// 2
+    elif allign == 'b':
+        line_coeff = lines - 1
+    else:
+        line_coeff = 0
+    if justify== 'c':
+        col_coeff = (cols  - len(msg))// 2
+    elif justify == 'r':
+        col_coeff = cols - 1
+    else:
+        col_coeff = 0    
+    return col_coeff, line_coeff
+
+
+def printer_by_lett(msg, style='', justify='l', timing=0.1):
+    '''
+        prints message msg letter by letter
+        gets: justify -- x positionement 
+              timing  -- time to wait between line printings
+              style   -- additional apperance settings
+    '''
+    col_coeff, line_coeff = get_padding(big_line, justify=justify)
+    for i in msg:
+        console.print(Padding(line,(0,col_coeff), style=style))
+        time.sleep(timing)
+    return ''
+
+
+def printer_by_line(msg, style='', justify='l', timing=0.1):
+    '''
+        prints message msg line by line
+        gets: justify -- x positionement 
+              timing  -- time to wait between line printings
+              style   -- additional apperance settings
+    '''
+    big_line = ''
+    msg = msg.split('\n')
+    for line in msg:
+        if len(line) > len(big_line):
+            big_line = line
+    col_coeff, line_coeff = get_padding(big_line, justify=justify)
+    for line in msg:
+        console.print(Padding(line,(0,col_coeff), style=style))
+        time.sleep(timing)
+    return ''
+        
+
+def echo(msg,m_type="normal", allign='t', justify='r',style='', clear_cli = False,*args,     **kwargs):
+    '''
+        clears console and prints message [m] of type [m_type] 
+        in specified position allign : y, justify :x.
+    '''
+    if clear_cli:
+        os.system('cls')
+        
+    col_coeff, line_coeff = get_padding(msg, allign=allign, justify=justify)     
+    if m_type == "normal":
+        # console.log(col_coeff,cols, len(m),m)
+        console.log(args)
+        if kwargs.by_printer:
+            console.print(Padding(printer_by_lett(msg,style),(line_coeff, col_coeff)))
+        else:
+            console.print(Padding(msg,(line_coeff, col_coeff)))
+        # time.sleep(4)
+        return
+    
+    elif m_type == "ask":
+        print('\n'*line_coeff)
+        print(' '*col_coeff, end='')
+        return Confirm.ask(msg)
+    
+
+def multiline_echo(m,m_type="normal", allign='t', justify='r',style='', clear_cli = False, **kwargs):
+    '''same as echo but for more then 1 line.'''
+    lines = m.split('\n')
+    for line in lines:
+        echo(line,m_type, allign, justify, style, clear_cli)
+
+
+def exit_conf():
+    '''Confirm exit choice.'''
+    conf = Confirm.ask('Do you want to exit?')
+    if conf:
+        console.print("Exited from user input")
+    return True
+
+
+def loader(char="|"):
+    '''displays loader.'''
+    os.system('cls')
+    cols, lines = get_console_size()
+    length = cols // 8 *2
+    col_coeff = (cols - length)// 2 -6
+    line_coeff = lines // 2 -3
+    
+    time.sleep(0.5)
+    loader_s = list(" "*length)
+    panel_title = "Loading"
+    for i in range(length+1):
+        if i == length:
+            panel_title = 'Completed'
+        os.system('cls')
+        load_panel = Panel.fit(''.join(loader_s), title=panel_title)
+        loader_obj = Padding(load_panel, (line_coeff, col_coeff), style="cyan")
+        print(loader_obj)
+        loader_s.pop()
+        loader_s.insert(0,char)
+        time.sleep(random()*0.2)
+    time.sleep(2.5)
+    os.system('cls')
---
+++ hangman.py
@@ -0,0 +1,108 @@
+from email import message
+import time 
+import random
+from rich.pretty import pprint
+from rich.console import Console
+from rich.prompt import Prompt, Confirm
+import os
+
+from exc import CustomException
+import cli
+import img
+
+console = Console()
+
+WORDS = ("python", "programming", "dictionnary", "editor", "list", "function")
+        
+LOGS = {
+    'START':"[green dim]Enter -e or --exit anytime to exit...[/]\n[bright_green]Guessing started..[/]",
+    'GUESS':"[blue]Turn #%var%\ncurrent word -- %var%[/]\n[bold red]Wrong letter[/]"}        
+
+
+def set_word(words):
+    """ Returns random word from WORDS list """
+    return random.choice(words)
+
+def create_repr(word):
+    """ Creats word representation in format: '_ _ _ _ _ _' """
+    word_repr = ""
+    for i in range(len(word)):
+        word_repr += '_'
+    return word_repr
+
+def update_repr(word,current_repr, guess):
+    """ Replaces ' _ ' to a letter  """
+    ind = 0
+    for let in word:
+        if let == guess:
+            current_repr = list(current_repr)
+            pprint(type(current_repr))
+            current_repr[ind] = let
+            return current_repr
+        ind += 1
+    return current_repr
+
+# TODO: docstrings
+def correct_letter_handler(word, guess, wrong_letters):
+    console.print("Succes!", style="magenta")
+    guessed_word = update_repr(word, guessed_word, guess)
+    console.print(f"current word -- \"{' '.join(guessed_word)}\"\n\nwrong letters -- \"{','.join(wrong_letters)}\"")
+    os.system('cls')
+    return guessed_word
+    
+def wrong_letter_handler(guess, guessed_word, wrong_letters):
+    dead = False
+    console.print("Wrong",style="red")
+    console.print(f"current word -- \"{' '.join(guessed_word)}\"\n\nwrong letters -- \"{','.join(wrong_letters)}\"")
+    os.system('cls')
+    guesses -= 1
+    if guess not in wrong_letters:
+        wrong_letters.append(guess)
+    if guesses == 0:
+        print('you died...')
+        dead = True
+    return guessed_word, wrong_letters, dead
+    
+def main():
+    # TODO: guesses < turns
+    guesses = 10
+    turns = 0
+    wrong_letters = []
+    try:
+        word = set_word(WORDS)
+        guessed_word = create_repr(word)
+        cli.multiline_echo(m=LOGS["START"],m_type='normal',allign='c',justify='c')
+        while True:
+            info_log = cli.compose_string(LOGS["GUESS"],f"{' '.join(guessed_word)}")
+            cli.multiline_echo(m=info_log,m_type='normal',allign='c',justify='c')
+            guess = Prompt.ask("Try one letter:") # User input TODO: protect this
+            if guess == "-e" or guess == "--exit":
+                    if cli.exit_conf(): return
+            if guess in word:
+                guessed_word = correct_letter_handler(word, guess, wrong_letters)
+            else:
+                guessed_word, wrong_letters, dead = wrong_letter_handler()
+                if dead:
+                    return
+            if '_' not in guessed_word:
+                console.print(f"Finished.\nThe word was - \"{word}\"")
+                return 
+            turns +=1
+            print("===================================")   
+    except Exception:
+        console.print_exception(show_locals = True)
+
+if __name__ == "__main__":
+    cols, lines = cli.get_console_size()
+    os.system('cls')
+    # cli.multiline_echo(img.logo, style="green blink", justify='c', by_printer=False)
+    start = cli.echo('Start Game', m_type='ask',allign='c', justify='c')
+    time.sleep(2)
+    os.system('cls')
+    if start:
+        cli.printer_by_line(img.logo, justify='c', style="green blink")
+        time.sleep(5)
+        cli.loader()
+        # os.system('cls')
+        main()
+    
